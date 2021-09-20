271b7db47fe2d2bd65e56de16e077762662e0c5e
Shubhangi Dharmik <85070726+Shubhangidharmik@users.noreply.github.com>
2021-09-20 17:22:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,118 @@
+import numpy as np
+import cv2
+import streamlit as st
+from tensorflow import keras
+from keras.models import model_from_json
+from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array
+
+# load model
+emotion_dict = {0:'angry', 1 :'happy', 2: 'neutral', 3:'sad', 4: 'surprise'}
+# load json and create model
+json_file = open('fer.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+classifier = model_from_json(loaded_model_json)
+
+# load weights into new model
+classifier.load_weights("fer2013.h5")
+
+#load face
+try:
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+except Exception:
+    st.write("Error loading cascade classifiers")
+
+
+
+
+
+def main():
+    # Face Analysis Application #
+    st.title("Real Time Face Emotion Detection Application")
+    activities = ["Home", "Webcam Face Detection", "About"]
+    choice = st.sidebar.selectbox("Select Activity", activities)
+    st.sidebar.markdown(
+        """ Real time face emotion detection """)
+    if choice == "Home":
+        html_temp_home1 = """<div style="background-color:#6D7B8D;padding:10px">
+                                            <h4 style="color:white;text-align:center;">
+                                            Face Emotion detection application.</h4>
+                                            </div>
+                                            </br>"""
+        st.markdown(html_temp_home1, unsafe_allow_html=True)
+        st.write("""
+                 The application has two functionalities.
+
+                 1. Real time face detection using web cam feed.
+
+                 2. Real time face emotion recognization.
+
+                 """)
+    elif choice == "Webcam Face Detection":
+        st.header("Webcam Live Feed")
+        st.write("Click on start to use webcam and detect your face emotion")
+        st.button("start")
+        cap = cv2.VideoCapture(0)
+
+        while True:
+            ret, test_img = cap.read()  # captures frame and returns boolean value and captured image
+            if not ret:
+                continue
+            gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+
+            faces_detected = face_cascade.detectMultiScale(gray_img, 1.32, 5)
+
+            for (x, y, w, h) in faces_detected:
+                cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
+                roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
+                roi_gray = cv2.resize(roi_gray, (48, 48))
+                img_pixels = image.img_to_array(roi_gray)
+                img_pixels = np.expand_dims(img_pixels, axis=0)
+                img_pixels /= 255
+
+                predictions = classifier.predict(img_pixels)
+
+                # find max indexed array
+                max_index = np.argmax(predictions[0])
+
+                emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+                predicted_emotion = emotions[max_index]
+
+                cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+            resized_img = cv2.resize(test_img, (1000, 700))
+            cv2.imshow('Facial emotion analysis ', resized_img)
+
+            if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
+                break
+
+        cap.release()
+        cv2.destroyAllWindows
+        #cv2.imshow('Facial emotion analysis ',resized_img)
+
+    elif choice == "About":
+        st.subheader("About this app")
+        html_temp_about1= """<div style="background-color:#6D7B8D;padding:10px">
+                                    <h4 style="color:white;text-align:center;">
+                                    Real time face emotion detection application.</h4>
+                                    </div>
+                                    </br>"""
+        st.markdown(html_temp_about1, unsafe_allow_html=True)
+
+        html_temp4 = """
+                             		<div style="background-color:#98AFC7;padding:10px">
+                             		<h4 style="color:white;text-align:center;">This Application is developed using Streamlit Framework, Opencv, Tensorflow and Keras library for demonstration purpose. </h4>
+                             		<h4 style="color:white;text-align:center;">Thanks for Visiting</h4>
+                             		</div>
+                             		<br></br>
+                             		<br></br>"""
+
+        st.markdown(html_temp4, unsafe_allow_html=True)
+
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    main()
